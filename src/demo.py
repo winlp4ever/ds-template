a9984befabf22b145794e338b68c78afc954f1c8
Le Ha Quang <winlp4ever@gmail.com>
2022-10-18 12:22:16 +0200

    feat(src/demo.py): implement POC tagger
    BREAKING CHANGE:

--- src/demo.py
+++ src/demo.py
@@ -1,3 +1,12 @@
+from typing import List, Tuple
+
 import spacy
 
-nlp = spacy.load('en')
+
+class POCTagger:
+    def __init__(self):
+        self._nlp = spacy.load('en')
+
+    def __call__(self, text: str) -> List[Tuple[str, str]]:
+        doc = self._nlp(text)
+        return [(tok.text, tok.pos_) for tok in doc]
